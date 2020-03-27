149f98c5a07aa6189f26f59b2af97bdec6cb56e0
Alejandro Toro <alejandro@kommit.co>
2020-03-27 18:09:26 -0500

    Add routes configuration

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -2,6 +2,8 @@ import bodyParser from 'koa-bodyparser';
 import Koa from 'koa';
 import koaCors from 'koa2-cors';
 
+import { registerRoutes } from './routes';
+
 async function serverSetUp() {
   const server: Koa = new Koa();
   middleWares(server);
@@ -11,6 +13,9 @@ async function serverSetUp() {
 function middleWares(server: Koa) {
   server.use(bodyParser());
   server.use(koaCors());
+
+  const routes = registerRoutes().routes();
+  server.use(routes);
 }
 
 async function startServer(server: Koa) {
