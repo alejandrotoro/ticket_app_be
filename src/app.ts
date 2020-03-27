673778dd8822beae70f1cbd9cd252ddd2a9f3ec2
Alejandro Toro <alejandro@kommit.co>
2020-03-27 18:03:17 -0500

    Add the database configuration setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import { databaseSetUp } from './setupDatabase';
 import { serverSetUp } from './setupServer';
 
 async function init() {
   await serverSetUp();
+  await databaseSetUp();
 }
 
 init();
