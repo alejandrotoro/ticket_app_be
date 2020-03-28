757e7735b90cb8047655a6af89bf8f4a80c6f2ce
Alejandro Toro <alejandro@kommit.co>
2020-03-27 19:39:21 -0500

    Add Schema, UserModel and User Interface

---
+++ src/models/user/User.schema.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema: mongoose.Schema = new mongoose.Schema({
+  username: { type: String },
+  password: { type: String },
+  role: { type: String},
+  date: { type: Date, default: Date.now }
+});
+
+export { userSchema };
